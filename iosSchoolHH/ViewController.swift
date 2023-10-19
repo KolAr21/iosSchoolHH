3fc64b42647001a25cef8d0fad35430ac1a3eac0
KolAr21 <arina.kolganova21@gmail.com>
2023-10-19 16:43:13 +0700

    create project

---
+++ iosSchoolHH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosSchoolHH
+//
+//  Created by Арина Колганова on 19.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
