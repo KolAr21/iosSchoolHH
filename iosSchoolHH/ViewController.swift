8c71e1a67d06602afe9d88a16612fae513c6b451
KolAr21 <arina.kolganova21@gmail.com>
2023-10-31 11:00:59 +0700

    update homework 2

--- iosSchoolHH/ViewController.swift
+++ iosSchoolHH/ViewController.swift
@@ -17,23 +17,27 @@ class ViewController: UIViewController {
         let characterGeneratorProtocol: CharacterGeneratorProtocol = CharacterGenerator()
         let characterProtocol = characterGeneratorProtocol.generate()
 
-        print(character.id,
-              character.name,
-              character.species,
-              character.image,
-              character.url,
-              character.episode,
-              character.gender,
-              character.status)
+        print(
+            character.id,
+            character.name,
+            character.species,
+            character.image,
+            character.url,
+            character.episode,
+            character.gender,
+            character.status
+        )
 
-        print(characterProtocol.id,
-              characterProtocol.name,
-              characterProtocol.species,
-              characterProtocol.image,
-              characterProtocol.url,
-              characterProtocol.episode,
-              characterProtocol.gender,
-              characterProtocol.status)
+        print(
+            characterProtocol.id,
+            characterProtocol.name,
+            characterProtocol.species,
+            characterProtocol.image,
+            characterProtocol.url,
+            characterProtocol.episode,
+            characterProtocol.gender,
+            characterProtocol.status
+        )
 
     }
 }
