7be424798313e829f8e19dbdde5e3fbb1b24ede0
KolAr21 <arina.kolganova21@gmail.com>
2023-11-02 20:50:11 +0700

    lesson 4

--- iosSchoolHH/ViewController.swift
+++ iosSchoolHH/ViewController.swift
@@ -17,28 +17,6 @@ class ViewController: UIViewController {
         let characterGeneratorProtocol: CharacterGeneratorProtocol = CharacterGenerator()
         let characterProtocol = characterGeneratorProtocol.generate()
 
-        print(
-            character.id,
-            character.name,
-            character.species,
-            character.image,
-            character.url,
-            character.episode,
-            character.gender,
-            character.status
-        )
-
-        print(
-            characterProtocol.id,
-            characterProtocol.name,
-            characterProtocol.species,
-            characterProtocol.image,
-            characterProtocol.url,
-            characterProtocol.episode,
-            characterProtocol.gender,
-            characterProtocol.status
-        )
-
         let generator = ColorGenerator(alpha: 1)
         generator.changeColorCodes { colorCodes in
             print(colorCodes)
@@ -47,5 +25,45 @@ class ViewController: UIViewController {
         generator.changeColor {
             [0]
         }
+
+        let color = ColorGenerator(alpha: 1)
+
+        let intArray = generator.convertToArray(element: 1)
+        let colorArr = generator.convertToArray(element: color)
+        let stringArray = generator.convertToArray(element: "hello world")
+
+        let palette = Palette(colors: [1, 2, 3, 4])
+        let anotherPalette = Palette(colors: ["red", "green"])
+        let colorPalette = Palette(colors: [color])
+        let pal = Palette(colors: colorArr)
+
+        let arrayInt = [1, 2, 3, 4, 5, 6, 7]
+        let result = arrayInt.filter { elem in
+            return elem % 2 == 0
+        }
+
+        let resultOne = arrayInt.filter { $0 % 2 == 0 }
+
+        let array = [0.1, 0.2, 0.5, 1]
+        let generators = array.map { ColorGenerator(alpha: $0)}
+        let test = array.map { alpha -> ColorGeneratorProtocol? in
+            guard alpha < 1 else { return nil }
+            return ColorGenerator(alpha: alpha)
+        }
+
+        let testOne: [ColorGenerator?] = array.map {
+            guard $0 < 1 else { return nil }
+
+            return ColorGenerator(alpha: $0)
+        }
+
+        let testTwo = testOne.compactMap { $0 }
+        let testThree = testOne.compactMap { $0?.alpha }
+        let testFour = testOne.map { $0?.alpha }
+
+        let resultReduce = arrayInt.reduce(0) { partialResult, element in
+            partialResult + element
+        }
+
     }
 }
