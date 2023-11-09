683adf6d62bf465bc5ab4a8327c71bac1e047469
KolAr21 <arina.kolganova21@gmail.com>
2023-11-09 16:50:51 +0700

    homework 5

---
+++ iosSchoolHH/Modules/Registration/RegistartionViewData.swift
@@ -0,0 +1,10 @@
+//
+//  RegistartionViewData.swift
+//  iosSchoolHH
+//
+//  Created by Арина Колганова on 09.11.2023.
+//
+
+import Foundation
+
+struct RegistrationViewData {}
---
+++ iosSchoolHH/Modules/Registration/RegistrationView.swift
@@ -0,0 +1,12 @@
+//
+//  RegistrationView.swift
+//  iosSchoolHH
+//
+//  Created by Арина Колганова on 09.11.2023.
+//
+
+import UIKit
+
+protocol RegistrationView: UIView {}
+
+class RegistrationViewImp: UIView, RegistrationView {}
