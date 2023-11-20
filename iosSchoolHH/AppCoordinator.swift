3110f7aaffe8f8419af9148e9487668aa6790a72
KolAr21 <arina.kolganova21@gmail.com>
2023-11-20 23:00:21 +0700

    lesson 9

--- iosSchoolHH/AppCoordinator.swift
+++ iosSchoolHH/AppCoordinator.swift
@@ -15,7 +15,7 @@ class AppCoordinator: BaseCoordinator<CoordinatorContext> {
 
     func start(window: UIWindow?) {
         self.window = window
-        setRoot(viewController: assembly.registrationCoordinator(onRegistrationSuccess: nil).make())
+        setRoot(viewController: assembly.authCoordinator().make())
     }
 
     private func setRoot(viewController: UIViewController?) {
