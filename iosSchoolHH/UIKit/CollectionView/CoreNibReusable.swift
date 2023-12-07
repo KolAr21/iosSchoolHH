b0a8982c5e9820b318762ed1078e4c9d6c9c161a
KolAr21 <arina.kolganova21@gmail.com>
2023-12-08 01:07:45 +0700

    lesson 14

---
+++ iosSchoolHH/UIKit/CollectionView/CoreNibReusable.swift
@@ -0,0 +1,24 @@
+//
+//  CoreNibReusable.swift
+//  iosSchoolHH
+//
+//  Created by Арина Колганова on 07.12.2023.
+//
+
+import UIKit
+
+protocol CoreNibReusable {
+    static var nib: UINib? { get }
+}
+
+extension CoreNibReusable where Self: UIView {
+    static var nib: UINib? {
+        UINib(nibName: String(describing: Self.self), bundle: Bundle(for: self))
+    }
+}
+
+extension CoreNibReusable {
+    static var nib: UINib? {
+        nil
+    }
+}
